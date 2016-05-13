7405ed5985f646da7b1263fedadfde7e2bd5c23e
Nur Rony <pro.nmrony@gmail.com>
2016-05-14 01:57:02 +0600

    lazy loading gulp plugins

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,31 @@
 import gulp from 'gulp'
-import eslint from 'gulp-eslint'
-import util from 'gulp-util'
-import print from 'gulp-print'
-import gulpif from 'gulp-if'
 import yargs from 'yargs'
+import lazyGulp from 'gulp-load-plugins'
 
 const args = yargs.argv
+const $ = lazyGulp({lazy: true})
+
 // Helper function
 const log = (msg) => {
   if (typeof msg === 'object') {
     for (const item in msg) {
       if (msg.hasOwnProperty(item)) {
-        util.log(util.colors.blue(item))
+        $.util.log($.util.colors.blue(item))
       }
     }
   } else {
-    util.log(util.colors.blue(msg))
+    $.util.log($.util.colors.blue(msg))
   }
 }
 
 // lint code
 const lintCode = () => {
-  log('Analyzing code...')
+  log('Analyzing code for error and styles...')
   return gulp.src(['src/**/*.js', '!node_modules/**'])
-    .pipe(gulpif(args.showLog, print()))
-    .pipe(eslint({extends: 'standard'}))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
+    .pipe($.if(args.showLog, $.print()))
+    .pipe($.eslint({extends: 'standard'}))
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError())
 }
 
 gulp.task('lint', lintCode)
